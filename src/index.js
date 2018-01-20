863337c9fa4f969fee48cf6b130df7a65da2ef4e
Christian Rivera <chris.arevir@gmail.com>
2018-01-20 10:43:44 -0800

    things!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './app.css';
+
+import { render } from 'react-dom';
+import React from 'react';
+
+const header = "Ticket";
+
+const requirementArea = <div>Hey</div>;
+
+const status = "ready pass fail thing";
+
+const commentThings = "Yo";
+
+const addButton = <button>+</button>;
+
+const removeButton = <button>-</button>;
+
+const page = (
+  <div>
+    {header}
+    {requirementArea}
+    {status}
+    {commentThings}
+    {addButton}
+    {removeButton}
+  </div>
+);
+
+const node = document.createElement('div');
+document.body.appendChild(node);
+
+render(
+  page,
+  node
+);
