e7b33c251c314cef38c5e0108549a33f44102702
Christian Rivera <chris.arevir@gmail.com>
2018-01-20 10:08:36 -0800

    initial move

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+module.exports = () => {
+  const prod = process.env.NODE_ENV === 'production';
+
+  const output = prod
+    ? { path: `${__dirname}/build/`, filename: '[name].js', libraryTarget: 'umd' }
+    : { path: '/', publicPath: 'http://localhost:5000/', filename: '[name].js', libraryTarget: 'umd' };
+
+  const htmlPlugin = new HtmlWebpackPlugin({
+    title: 'Stage Me',
+    template: './index.html'
+  });
+
+  const webAppManifestPlugin = new WebpackPwaManifest({
+    'theme-color': '#00ced1',
+    background_color: '#00ced1',
+    description: 'Lets get you a staging server!',
+    display: 'standalone',
+    icons: [{
+      src: path.resolve('src/assets/icon.png'),
+      sizes: [16, 24, 32, 48, 64, 96, 128, 256, 512]
+    }],
+    name: 'Stage Me',
+    short_name: 'Stage Me',
+    start_url: '/', // TODO: Put to the right place
+    theme_color: '#00ced1',
+    viewport: 'width=device-width, initial-scale=1'
+  });
+
+  const plugins = prod
+    ? [
+      htmlPlugin,
+      new SWPrecacheWebpackPlugin(),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        exclude: [/\.min\.js$/gi],
+        compress: {
+          pure_getters: true,
+          screw_ie8: true,
+          unsafe_comps: true,
+          unsafe: true,
+          warnings: false
+        },
+        mangle: true,
+        output: { comments: false },
+        sourceMap: false
+      }),
+      webAppManifestPlugin
+    ] : [
+      htmlPlugin,
+      new FriendlyErrorsWebpackPlugin(),
+      new SWPrecacheWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new WebpackNotifierPlugin({ alwaysNotify: false }),
+      webAppManifestPlugin
+    ];
+
+  return {
+    devServer: {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      host: '0.0.0.0',
+      hot: true,
+      port: 5000
+    },
+
+    devtool: 'eval',
+
+    entry: './src/index.js',
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          include: [path.resolve(__dirname, 'src')]
+        },
+        {
+          test: /\.s?[ca]ss$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+            { loader: 'postcss-loader' }
+          ]
+        }
+      ]
+    },
+
+    output,
+
+    plugins,
+
+    resolve: { extensions: ['.js', '.jsx', '.css', '.scss'] }
+  };
+};
